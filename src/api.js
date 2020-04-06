4ccd02093292c3c6966039c25cafb6531f13f71c
Seongmin0801 <seongmin0801@gmail.com>
2020-04-06 17:51:24 +0900

    completed #4.3

---
+++ src/api.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+});
+
+api.interceptors.request.use((config) => {
+    config.params = config.params || {};
+    config.params['api_key'] = 'e888a49335fbd825c9bbcf82da7981f8';
+    config.params['language'] = 'en-US';
+    return config;
+});
+
+export const moviesAPI = {
+    nowPlaying: () => api.get('movie/now_playing'),
+    upcoming: () => api.get('movie/upconming'),
+    popular: () => api.get('movie/popular'),
+    movieDetail: (id) =>
+        api.get(`movie/${id}`, {
+            params: {
+                append_to_response: 'videos',
+            },
+        }),
+    search: (term) =>
+        api.get('search/movie', {
+            params: {
+                query: encodeURIComponent(term),
+            },
+        }),
+};
+
+export const tvAPI = {
+    topRated: () => api.get('tv/top_rated'),
+    popular: () => api.get('tv/popular'),
+    airingToday: () => api.get('tv/airing_today'),
+    showDetail: (id) =>
+        api.get(`tv/${id}`, {
+            params: {
+                append_to_response: 'videos',
+            },
+        }),
+    search: (term) =>
+        api.get('search/tv', {
+            params: {
+                query: encodeURIComponent(term),
+            },
+        }),
+};
