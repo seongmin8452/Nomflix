d18b062ce5f8c126dd117584b24c9c6b9713575a
Seongmin0801 <seongmin0801@gmail.com>
2020-04-12 23:40:01 +0900

    Updates

---
+++ precache-manifest.dbf6fe512f578dcbf4dcba795042b6ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d589cd60784023b5b18323ba85bf315",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "84b26bf4d00ec91ece20",
+    "url": "/nomflix/static/js/2.9207eb57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nomflix/static/js/2.9207eb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9f51a41fc906eda2b3",
+    "url": "/nomflix/static/js/main.54a61c29.chunk.js"
+  },
+  {
+    "revision": "7f0b2571cc398dde6626",
+    "url": "/nomflix/static/js/runtime-main.181cbd9a.js"
+  },
+  {
+    "revision": "63115ec273aeb5778c9d1119eb4176fb",
+    "url": "/nomflix/static/media/IMDb.63115ec2.svg"
+  },
+  {
+    "revision": "6bc11c2ea87b959eb371dd2e91f4fb5b",
+    "url": "/nomflix/static/media/Loading.6bc11c2e.gif"
+  },
+  {
+    "revision": "13526e1eb815abe5be9b3acb01d9dccc",
+    "url": "/nomflix/static/media/NoVideo.13526e1e.png"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
